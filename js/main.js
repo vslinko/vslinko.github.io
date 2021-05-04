44f55f2e718ca0cb096c5e2a6973642ce15c0182
Viacheslav Slinko <vslinko@yahoo.com>
2021-05-04 16:51:02 +0300

    Update

---
+++ js/main.js
@@ -0,0 +1,46 @@
+async function loadPage(url) {
+  const res = await fetch(url);
+  const data = await res.text();
+  const doc = new DOMParser().parseFromString(data, "text/html");
+
+  for (const selector of ["title", ".garden_layout__main", ".toc"]) {
+    document.querySelector(selector).innerHTML = doc.querySelector(
+      selector
+    ).innerHTML;
+  }
+
+  const disqusUrl = doc.querySelector("#disqus_thread").dataset.url;
+  window.disqus_config = function () {
+    this.page.url = disqusUrl;
+  };
+
+  window.DISQUS.reset({
+    reload: true,
+  });
+}
+
+document.addEventListener("click", async (e) => {
+  if (e.target.tagName !== "A") {
+    return;
+  }
+
+  const url = new URL(e.target.href);
+
+  if (
+    url.origin !== location.origin ||
+    !url.pathname.startsWith("/garden/") ||
+    url.pathname === location.pathname
+  ) {
+    return;
+  }
+
+  e.preventDefault();
+
+  await loadPage(url);
+
+  history.pushState(null, document.querySelector("title").innerHTML, url);
+});
+
+window.onpopstate = async () => {
+  await loadPage(new URL(location));
+};
